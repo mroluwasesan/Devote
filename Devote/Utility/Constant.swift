55aee587ed3996eadecc4f4b6e9fe1fddc716ce6
mroluwasesan <mr.oluwasesan@gmail.com>
2023-05-08 21:03:59 +0100

    SettingUp the app Core Data environment

---
+++ Devote/Utility/Constant.swift
@@ -0,0 +1,20 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Negatron on 08/05/2023.
+//
+
+import SwiftUI
+
+// FORMATTER
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+// MARK: - UX
