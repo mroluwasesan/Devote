503268e07d6879e1e2886bc1b7e3ba6e842905c6
mroluwasesan <mr.oluwasesan@gmail.com>
2023-05-10 17:25:31 +0100

    Creating  To-do  app from Xcode's Core Data templete

--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -11,9 +11,15 @@ import CoreData
 struct ContentView: View {
     // MARK: - PROPERTY
     
+    @State var task: String = ""
+    
+    private var isButtonDisabled: Bool{
+        task.isEmpty
+    }
+    
     //  FETCHING DATA
     @Environment(\.managedObjectContext) private var viewContext
-
+    
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
@@ -25,66 +31,98 @@ struct ContentView: View {
         withAnimation {
             let newItem = Item(context: viewContext)
             newItem.timestamp = Date()
-
+            newItem.task = task
+            newItem.completion = false
+            newItem.id = UUID()
+            
             do {
                 try viewContext.save()
             } catch {
-               
+                
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
+            
+            task = ""
+            hideKeyboard()
         }
     }
-
+    
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
             offsets.map { items[$0] }.forEach(viewContext.delete)
-
+            
             do {
                 try viewContext.save()
             } catch {
-               
+                
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
         }
     }
     
-
+    
     // MARK: - BODY
     var body: some View {
         NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+            VStack {
+                VStack(spacing: 16){
+                    TextField("New Task", text: $task)
+                        .padding()
+                        .background(
+                            Color(.systemGray5)
+                        )
+                        .cornerRadius(10)
+                    Button(action: {
+                        addItem()
+                    }, label: {
+                        Spacer()
+                        Text("SAVE")
+                        Spacer()
+                    })
+                    .disabled(isButtonDisabled)
+                    .padding()
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .background(isButtonDisabled ? Color.gray : Color.pink)
+                    .cornerRadius(10)
+                }// : VSTACK
+                .padding()
+                List {
+                    ForEach(items) { item in
+                        VStack (alignment: .leading){
+                                Text(item.task ?? "" )
+                                    .font(.headline)
+                                    .fontWeight(.bold)
+                                
+                                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                                    .font(.footnote)
+                                    .foregroundColor(.gray)
+                            }//: LIST ITEM
                     }
-                }
-                .onDelete(perform: deleteItems)
-            }// : LIST
+                    .onDelete(perform: deleteItems)
+                }// : LIST
+            }// : VSTACK
+            .navigationBarTitle("Daily Tasks", displayMode: .large)
             .toolbar {
+                #if os(iOS)
                 ToolbarItem(placement: .navigationBarTrailing) {
                     EditButton()
                 }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }// : ToolBar
-            Text("Select an item")
-        }// : Navigation
+                #endif
+                
+            }// : TOOLBAR
+            
+        }// : NAVIGATION
+    }//BODY
+    }//CONTENT VIEW
+    
+    
+    // MARK: - PREVIEW
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        }
     }
-
-}
-
-
-// MARK: - PREVIEW
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
-    }
-}
