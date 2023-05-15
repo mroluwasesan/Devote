b8dce25a8b9577a6bae5ceaab86840814810277d
mroluwasesan <mr.oluwasesan@gmail.com>
2023-05-15 23:29:43 +0100

    Creating Custom Visual Effect

--- Devote/View/BlankView.swift
+++ Devote/View/BlankView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct BlankView: View {
     // MARK: - PROPERTY
     
+    var backgroundColor: Color
+    var backgroundOpacity: Double
     
     
     // MARK: - BODY
@@ -19,8 +21,10 @@ struct BlankView: View {
         Spacer()
       }
       .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
-      .background(Color.black)
-      .opacity(0.5)
+      .background(backgroundColor)
+      .opacity(backgroundOpacity)
+      .blendMode(.overlay)
+      .edgesIgnoringSafeArea(.all)
       .edgesIgnoringSafeArea(.all)
     }
   }
@@ -29,7 +33,9 @@ struct BlankView: View {
 
   struct BlankView_Previews: PreviewProvider {
     static var previews: some View {
-      BlankView()
+        BlankView(backgroundColor: Color.black, backgroundOpacity: 0.3)
+          .background(BackgroundImageView())
+          .background(backgroundGradient.ignoresSafeArea(.all))
         
     }
   }
--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
     @AppStorage("isDarkMode") private var isDarkMode: Bool = false
     @State var task: String = ""
     @State private var showNewTaskItem: Bool = false
-    
+    @State private var offset: Bool = false
     //  FETCHING DATA
     @Environment(\.managedObjectContext) private var viewContext
     
@@ -110,11 +110,16 @@ struct ContentView: View {
                     .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.3), radius: 12)
                     .padding(.vertical,0)
                     .frame(maxWidth: 640)
+                    
                 }// : VSTACK
+                .blur(radius: showNewTaskItem ? 8.0 : 0, opaque: false)
+                .transition(.move(edge: .bottom))
+                .animation(.easeOut(duration: 0.5))
+                
                 
                 // MARK: - NEW TASK ITEM
                 if showNewTaskItem{
-                    BlankView()
+                    BlankView(backgroundColor: isDarkMode ? Color.black : Color.gray, backgroundOpacity: isDarkMode ? 0.3: 0.5)
                         .onTapGesture {
                           withAnimation() {
                             showNewTaskItem = false
@@ -126,7 +131,7 @@ struct ContentView: View {
             }// : ZSTACK
             .navigationBarTitle("Daily Tasks", displayMode: .large)
             .navigationBarHidden(true)
-            .background(BackgroundImageView())
+            .background(BackgroundImageView().blur(radius: showNewTaskItem ? 8.0 : 0, opaque: false))
             .background(
                 backgroundGradient.ignoresSafeArea(.all)
             )
