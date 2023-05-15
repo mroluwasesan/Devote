20ce0f0ea534c11d680032e5f9dcb19173dbea4a
mroluwasesan <mr.oluwasesan@gmail.com>
2023-05-15 18:16:26 +0100

    Imporve the individual task View

--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -101,16 +101,7 @@ struct ContentView: View {
                     // MARK: - TASK
                     List {
                         ForEach(items) { item in
-                            VStack (alignment: .leading){
-                                Text(item.task ?? "" )
-                                    .font(.headline)
-                                    .fontWeight(.bold)
-                                
-                                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                                    .font(.footnote)
-                                    .foregroundColor(.gray)
-                                
-                            }//: LIST ITEM
+                            ListRowItemView(item: item)
                         }
                         .onDelete(perform: deleteItems)
                     }// : LIST
