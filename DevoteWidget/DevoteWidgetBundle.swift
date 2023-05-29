e3502bad82f055092c78c1c215b5188e36c3b1b7
mroluwasesan <mr.oluwasesan@gmail.com>
2023-05-29 14:38:49 +0100

    Working on widget for the app

---
+++ DevoteWidget/DevoteWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  DevoteWidgetBundle.swift
+//  DevoteWidget
+//
+//  Created by Negatron on 27/05/2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DevoteWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DevoteWidget()
+        DevoteWidgetLiveActivity()
+    }
+}
